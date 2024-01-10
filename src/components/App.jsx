fd32838dd56c7f9cb8c3697f171605d5fe9726d1
DyshliukAnna <113441385+DyshliukAnna@users.noreply.github.com>
2024-01-10 23:21:10 +0200

    done Searchbar for 90 per cent

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect} from 'react';
 import ImageGallery from "./ImageGallery/ImageGallery";
 import Button from "./Button/Button";
 import Loader from "./Loader/Loader";
-import Searchbar from "./Searchbar/Searchbar";
+import SearchBar from "./SearchBar/SearchBar";
 
 import fetchPhotos from 'components/API/API';
 
@@ -38,12 +38,24 @@ import fetchPhotos from 'components/API/API';
     };
     fetchData();
   }, [searchQuery, page]);
+
+  const incrementPage = () => {
+    setPage(prevPage => prevPage + 1);
+  };
+
+  const handleSearchSubmit = query => {
+    setSearchQuery(query);
+    setPage(1);
+    setData([]);
+    setShowBtn(false);
+  };
+
   return (
     <>
-    <Searchbar onSubmit={this.handleSearchSubmit} />
+    <SearchBar onSubmit={handleSearchSubmit} />
      <div className="container">
       <ImageGallery dataImages={data} />
-          {showBtn && <Button onClick={this.incrementPage} />}
+          {showBtn && <Button onClick={incrementPage} />}
           {status === 'pending' && <Loader />}
           {status === 'rejected' && (
             <div className="info">
@@ -61,42 +73,26 @@ import fetchPhotos from 'components/API/API';
 
 
 
-  async componentDidUpdate(_, prevState) {
-    const { searchQuery, page } = this.state;
-    if (searchQuery !== prevState.searchQuery || prevState.page !== page) {
-      try {
-        this.setState({ status: 'pending' });
-        const { totalHits, hits } = await fetchPhotos(searchQuery, page);
-        this.setState(prevState => ({
-          data: [...prevState.data, ...hits],
-          showBtn: page < Math.ceil(totalHits / 12),
-          status: 'resolved',
-        }));
-      } catch (error) {
-        this.setState({
-          error,
-          status: 'rejected',
-        });
-        console.log(error.message);
-      }
-    }
-  }
+  // async componentDidUpdate(_, prevState) {
+  //   const { searchQuery, page } = this.state;
+  //   if (searchQuery !== prevState.searchQuery || prevState.page !== page) {
+  //     try {
+  //       this.setState({ status: 'pending' });
+  //       const { totalHits, hits } = await fetchPhotos(searchQuery, page);
+  //       this.setState(prevState => ({
+  //         data: [...prevState.data, ...hits],
+  //         showBtn: page < Math.ceil(totalHits / 12),
+  //         status: 'resolved',
+  //       }));
+  //     } catch (error) {
+  //       this.setState({
+  //         error,
+  //         status: 'rejected',
+  //       });
+  //       console.log(error.message);
+  //     }
+  //   }
+  // }
 
 
-  incrementPage = () => {
-    this.setState(({ page }) => ({ page: page + 1 }));
-  };
-
-  handleSearchSubmit = searchQuery => {
-    this.setState({
-      searchQuery,
-      page: 1,
-      data: [],
-      showBtn: false,
-    });
-  };
-
-
-  render(){
-    const { data, status, showBtn } = this.state;
  
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,60 +1,51 @@
-import css from './Searchbar.module.css';
-import { Component } from 'react';
+import css from './SearchBar.module.css';
+import { useState } from 'react';
 import Notiflix from 'notiflix';
 import { AiOutlineSearch } from 'react-icons/ai';
+// import { getValue } from '@testing-library/user-event/dist/utils';
 
-class Searchbar extends Component {
-    state = {
-        searchQuery: '',
-      };
-      handleInputChange = ({ target: { name, value } }) => {
-        this.setState({
-          [name]: value,
-        });
-      };
-    
-      handleSearchSubmit = e => {
-        e.preventDefault();
-        if (this.state.searchQuery.trim() === '') {
-          Notiflix.Notify.info('Search string is empty!');
-          return;
-        }
-        this.props.onSubmit(this.state.searchQuery);
-        this.reset();
-      };
-    
-      reset = () => {
-        this.setState({
-          searchQuery: '',
-        });
-      };
-    
+const SearchBar = ({ onSubmit }) => {
+  const [searchQuery, setsearchQuery] = useState('');
 
-  render() {
-    return (
-      <>
-        <header className ={css.Searchbar}>
-          <form className={css.Form} onSubmit={this.handleSearchSubmit}>
-            <button type="submit" className={css.Button}>
-             <AiOutlineSearch
-              style={{ width: '25px', height: '25px' }}
-             />
-            </button>
+  const handleInputChange = ({ target: { value } }) => {
+    setsearchQuery(value);
+  };
+  const handleSearchSubmit = e => {
+    e.preventDefault();
+    if (searchQuery.trim() === '') {
+      Notiflix.Notify.info('Search string is empty!');
+      return;
+    }
+    onSubmit(searchQuery);
+    reset();
+  };
 
-            <input
-              className={css.Input}
-              type="text"
-              autoComplete="off"
-              name="searchQuery"
-              value={this.state.searchQuery}
-              autoFocus
-              placeholder="Search images and photos"
-              onChange={this.handleInputChange}
-            />
-          </form>
-        </header>
-      </>
-    );
-  }
-}
-export default Searchbar;
+  const reset = () => {
+    setsearchQuery('');
+  };
+
+  return (
+    <>
+      <header className={css.SearchBar}>
+        <form className={css.Form} onSubmit={handleSearchSubmit}>
+          <button type="submit" className={css.Button}>
+            <AiOutlineSearch style={{ width: '25px', height: '25px' }} />
+          </button>
+
+          <input
+            className={css.Input}
+            type="text"
+            autoComplete="off"
+            name="searchQuery"
+            value={searchQuery}
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={handleInputChange}
+          />
+        </form>
+      </header>
+    </>
+  );
+};
+
+export default SearchBar;
